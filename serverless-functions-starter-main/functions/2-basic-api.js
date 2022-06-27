4636cff5eb3c318bb9c990ed5c53577bd1266477
Stephanie Cobble <cobble.stephanie@gmail.com>
2022-06-27 15:37:05 -0700

    project 3, airtable, complete

--- serverless-functions-starter-main/functions/2-basic-api.js
+++ serverless-functions-starter-main/functions/2-basic-api.js
@@ -3,6 +3,10 @@ const items = require('../assets/data')
 
 exports.handler = async (event, context, cb) => {
     return {
+        // headers: {
+        //     'Access-Control-Allow-Origin': '*',
+        // },
+        //^^to make the app more accessible 
         statusCode: 200,
         body: JSON.stringify(items),
     };
